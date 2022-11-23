79c6692e7f4a083cabe14fcb177bc7ee8e675fbb
Karsten Bruckmann <karsten.bruckmann@check24.de>
2022-11-23 19:27:08 +0100

    [main] Auto open index, when needed

--- src/app/components/rooster-index/rooster-index.component.ts
+++ src/app/components/rooster-index/rooster-index.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { filter, firstValueFrom, switchMap } from 'rxjs';
 import { RoostersService } from '../../services/roosters.service';
@@ -12,7 +12,7 @@ import { StateService } from '../../services/state.service';
   templateUrl: './rooster-index.component.html',
   styleUrls: ['./rooster-index.component.scss'],
 })
-export class RoosterIndexComponent {
+export class RoosterIndexComponent implements AfterViewInit {
   constructor(
     private roostersService: RoostersService,
     private state: StateService
@@ -22,6 +22,20 @@ export class RoosterIndexComponent {
     filter((r): r is string => !!r),
     switchMap(rooster => this.roostersService.rooster$(rooster))
   );
+
+  @ViewChild('modal') public modal?: HTMLIonModalElement;
+
+  public open(): void {
+    this.modal?.present();
+  }
+
+  public async ngAfterViewInit(): Promise<void> {
+    const rooster = await firstValueFrom(this.state.rooster$);
+    const sheet = await firstValueFrom(this.state.sheet$);
+    if (!!rooster && !sheet) {
+      this.modal?.present();
+    }
+  }
 
   public async showSection(
     forceName: string,
