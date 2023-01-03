6d46a68be4518503e14d32afb72d984317a7810d
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-01-03 15:00:00 +0100

    Add updater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { IonicModule } from '@ionic/angular';
+import { filter } from 'rxjs';
 import { RoosterIndexComponent } from './components/rooster-index/rooster-index.component';
 import { RoosterSelectorComponent } from './components/rooster-selector/rooster-selector.component';
 import { RoosterComponent } from './components/rooster/rooster.component';
@@ -18,4 +20,16 @@ import { RoosterComponent } from './components/rooster/rooster.component';
     RoosterSelectorComponent,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(updates: SwUpdate) {
+    updates.versionUpdates
+      .pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+      )
+      .subscribe(() => {
+        if (confirm('Neue App-Version verfügbar. Jetzt neu laden?')) {
+          updates.activateUpdate().then(() => document.location.reload());
+        }
+      });
+  }
+}
